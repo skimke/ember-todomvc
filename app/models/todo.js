19ed514c9b0243bd70fc58da44c0abf325f69319
Stella Kim <stella.kim@icloud.com>
2015-05-05 18:11:05 -0400

    some static data for the todo model

--- app/models/todo.js
+++ app/models/todo.js
@@ -1,6 +1,24 @@
 import DS from 'ember-data';
 
-export default DS.Model.extend({
+export default DS.Model.extend({ //ember-cli uses filenames to create modules
 	title: DS.attr('string'),
 	isCompleted: DS.attr('boolean')
+}).reopenClass({
+    FIXTURES: [
+        {
+            id: 1, //required for static data
+            title: "Complete Ember.js Tutorial",
+            isCompleted: false
+        },
+        {
+            id: 2,
+            title: "Checkout some more ember stuff",
+            isCompleted: true
+        },
+        {
+            id: 3,
+            title: "Make my own stuff with Ember",
+            isCompleted: false
+        }
+    ]
 });
