0d2f7d7aceb1e9f22d658154b85fccf1a3f68c05
Stella Kim <stella.kim@icloud.com>
2015-05-05 18:56:27 -0400

    create method in the todo controller

--- app/controllers/todos.js
+++ app/controllers/todos.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
-export default Ember.Controller.extend({
+export default Ember.ArrayController.extend({ //need to specify controller type
+	actions: {
+		createTodo: function(newTitle) { //handlebar automatically passes in values as this variable
+			// create a new todo
+			var todo = this.store.createRecord('todo', {
+				title: newTitle,
+				isCompleted: false
+			});
+
+			// clear the input text field
+			this.set('newTitle', '');
+
+			// save the new todo
+			todo.save();
+		}
+	}
 });
