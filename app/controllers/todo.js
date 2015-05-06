223196f3746baef7254faa68c235b1cb39ffdf36
Stella Kim <stella.kim@icloud.com>
2015-05-06 18:15:31 -0400

    adding the acceptChanges handlebar helpers and the action in the controller

--- app/controllers/todo.js
+++ app/controllers/todo.js
@@ -4,6 +4,21 @@ export default Ember.ObjectController.extend({ //must specify ObjectController r
 	actions: {
 		editTodo: function() {
 			this.set('isEditing', true);
+		},
+		acceptChanges: function() {
+			this.set('isEditing', false); //done editing with this action, set state to false
+
+			if(Ember.isEmpty(this.get('model.title')) ) {//if the todo with the title is empty
+				this.send('removeTodo'); //if so, call the removeTodo action
+			} else {
+				this.get('model').save(); //if not, save in the model
+			}
+
+		},
+		removeTodo: function() {
+			var todo = this.get('model'); // get the model
+			todo.deleteRecord(); //delete the record
+			todo.save(); //save the deletion
 		}
 	}//,
 
