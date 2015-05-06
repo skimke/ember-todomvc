35d21bf546ad7250297e3c9132b290d7b5448f15
Stella Kim <stella.kim@icloud.com>
2015-05-06 18:32:33 -0400

    making both active and complete routes render todos.index; specifying which controller, and which todos to filter for in the model

--- app/routes/todos/active.js
+++ app/routes/todos/active.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	model: function() {
+		return this.store.filter('todo', function(todo) {
+			return !todo.get('isCompleted'); //return the todos that don't have isCompleted value of true
+		});
+	},
+	renderTemplate: function(controller) {
+		this.render('todos.index', {controller: controller}); //use the todos ArrayController and its index
+	}
+
 });
--- app/routes/todos/complete.js
+++ app/routes/todos/complete.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	model: function() {
+		this.store.filter('todo', function(todo) {
+			return todo.get('isCompleted'); //return the todos with isCompleted value of true
+		});
+	},
+	renderTemplate: function(controller) {
+		this.render('todos.index', {controller: controller}); // use the ArrayController todos to render the index
+	}
 });
