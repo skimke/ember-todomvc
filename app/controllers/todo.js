750244fcc382c8f693f080352c121d412384b172
Stella Kim <stella.kim@icloud.com>
2015-05-06 18:01:49 -0400

    adding the editTodo action to todo controller

--- app/controllers/todo.js
+++ app/controllers/todo.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
-export default Ember.Controller.extend({
+export default Ember.ObjectController.extend({ //must specify ObjectController rather than default Controller
+	actions: {
+		editTodo: function() {
+			this.set('isEditing', true);
+		}
+	}
 });
