78cd2c30267cf6fb6e17fa4304fcf204c647af90
Stella Kim <stella.kim@icloud.com>
2015-05-06 18:41:38 -0400

    links to each filtered lists point to created routes; fixed the complete route

--- app/routes/todos/complete.js
+++ app/routes/todos/complete.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	model: function() {
-		this.store.filter('todo', function(todo) {
+		return this.store.filter('todo', function(todo) {
 			return todo.get('isCompleted'); //return the todos with isCompleted value of true
 		});
 	},
