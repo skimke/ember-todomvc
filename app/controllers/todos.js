3ebb0714c5ac46c88b56543cd0c9811d55d9492e
Stella Kim <stella.kim@icloud.com>
2015-05-07 17:23:30 -0400

    adding remaining and inflection computed properties to the todos controller

--- app/controllers/todos.js
+++ app/controllers/todos.js
@@ -15,5 +15,13 @@ export default Ember.ArrayController.extend({ //need to specify controller type
 			// save the new todo
 			todo.save();
 		}
-	}
+	},
+	remaining: function() {
+		return this.filterBy('isCompleted', false).get('length'); //count for all todos with isCompleted value of false
+	}.property('@each.isCompleted'), //a computed property that will watch each todo for changes to the isCompleted value
+	inflection: function() {
+		var remaining = this.get('remaining');
+		return (remaining === 1) ? 'item' : 'items';
+	}.property('remaining') //computed property that watches the remaining property
+
 });
