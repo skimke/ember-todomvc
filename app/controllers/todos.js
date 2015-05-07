83ecda5f2c5b696cb36b53afcfc6c34dae2c70d0
Stella Kim <stella.kim@icloud.com>
2015-05-07 18:01:48 -0400

    added a property to the todos controller that, on interaction with the checkbox, will check the isCompleted value of every todo and set them all to whichever checkbox value

--- app/controllers/todos.js
+++ app/controllers/todos.js
@@ -28,17 +28,31 @@ export default Ember.ArrayController.extend({ //need to specify controller type
 		return this.filterBy('isCompleted', false).get('length'); //count for all todos with isCompleted value of false
 	}.property('@each.isCompleted'), //a computed property that will watch each todo for changes to the isCompleted value
 
+	completed: function() {
+		return this.filterBy('isCompleted', true).get('length');
+	}.property('@each.isCompleted'),
+
+	allAreDone: function(key, value) {
+		console.log(key + ": " + value);
+
+		if (value === undefined) { //if the checkbox value is undefined
+			return this.get('length') > 0 && this.isEvery('isCompleted', true);
+			//are there any todos, and if every one of them's isCompleted value of true
+		} else {
+			this.setEach('isCompleted', value); //if checkbox value is defined aka it was clicked, set each todo's value as either true or false (checked aka true by default)
+			this.invoke('save'); //then save
+			return value;
+		}
+	}.property('@each.isCompleted'),
+
 	inflection: function() {
 		var remaining = this.get('remaining');
 		return (remaining === 1) ? 'item' : 'items';
 	}.property('remaining'), //computed property that watches the remaining property
 
-	completed: function() {
-		return this.filterBy('isCompleted', true).get('length');
-	}.property('@each.isCompleted'),
-
 	hasCompleted: function() {
 		return this.get('completed') > 0; //the var completed in completed
 	}.property('completed') //watching the completed property
+
 
 });
